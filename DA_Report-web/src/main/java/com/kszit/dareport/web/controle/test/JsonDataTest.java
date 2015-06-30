2f52847c8916ee6f7f3e2234b034521b33c0029a
hanxiaowei <kszit@163.com>
2015-06-30 15:48:23 +0800

    添加web组件；

---
+++ DA_Report-web/src/main/java/com/kszit/dareport/web/controle/test/JsonDataTest.java
@@ -0,0 +1,22 @@
+package com.kszit.dareport.web.controle.test;
+
+import com.kszit.stu.webInvoke.web.controle.Controle;
+import com.kszit.stu.webInvoke.web.controle.Page;
+
+/**
+ * 返回json格式数据的测试类
+ * @author Administrator
+ *
+ */
+public class JsonDataTest extends Controle{
+	/**
+	 * 调用：dareport/dit.do?a=JsonDataTest&m=getJsonData
+	 */
+	public void getJsonData(){
+		Page page = new Page();
+		page.setJsonContent("dddd");
+		super.setPage(page);
+	}
+	
+	
+}
